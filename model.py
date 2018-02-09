ab967dd4188a8367e6288b0f153e2c0c1f28b87e
vagrant <vagrant@vagrant.vm>
2018-02-09 01:53:51 +0000

    Can show invitees on event page. -Can show invited events to user page. -Working on Google Calendar API.

--- model.py
+++ model.py
@@ -7,130 +7,128 @@ db = SQLAlchemy()
 ##### Model definitions #####
 
 class User(db.Model):
-	"""Users."""
+    """Users."""
 
-	__tablename__ = "users"
+    __tablename__ = "users"
 
-	user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-	name = db.Column(db.String(64), nullable=False)
-	email = db.Column(db.String(64), nullable=False, unique=True)
-	password = db.Column(db.String(64), nullable=False, unique=True)
-	phone = db.Column(db.Integer, nullable=True)
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(64), nullable=False, unique=True)
+    password = db.Column(db.String(64), nullable=False, unique=True)
+    phone = db.Column(db.Integer, nullable=True)
+    image = db.Column(db.String(64), nullable=True)
 
-	friends = db.relationship('User', secondary='friendships',
-							  primaryjoin='User.user_id==Friendship.friend_1_id',
-							  secondaryjoin='User.user_id==Friendship.friend_2_id')
+    friends = db.relationship('User', secondary='friendships',
+                              primaryjoin='User.user_id==Friendship.friend_1_id',
+                              secondaryjoin='User.user_id==Friendship.friend_2_id')
 
-	#must add friendship twice, one way, then the other
+    #must add friendship twice, one way, then the other
 
 
-	def __repr__(self):
-		"""Provide helpful representation when printed"""
+    def __repr__(self):
+        """Provide helpful representation when printed"""
 
-		return "< User user_id={} name={} email={} >".format(self.user_id, self.name, self.email)
+        return "< User user_id={} name={} email={} >".format(self.user_id, self.name, self.email)
 
 
 class Event(db.Model):
-	"""Event."""
+    """Event."""
 
-	__tablename__ = "events"
+    __tablename__ = "events"
 
-	event_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-	title = db.Column(db.String(64), nullable=False)
-	start_at = db.Column(db.DateTime, nullable=False)
-	end_at = db.Column(db.DateTime, nullable=False)
-	creator_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
-	public = db.Column(db.Boolean, nullable=True)
+    event_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    title = db.Column(db.String(64), nullable=False)
+    start_at = db.Column(db.DateTime, nullable=False)
+    end_at = db.Column(db.DateTime, nullable=False)
+    creator_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    public = db.Column(db.Boolean, nullable=True)
 
-	creator = db.relationship('User', backref=db.backref('created_events'))
+    creator = db.relationship('User', backref=db.backref('created_events'))
 
-	invitees = db.relationship('User', secondary='invitations', backref='invited_events')
-	#go first to inivation class to get user_id, then go to users
+    invitees = db.relationship('User', secondary='invitations', backref='invited_events')
+    #go first to inivation class to get user_id, then go to users
 
-	
-	def __repr__(self):
-		"""Provide helpful representation when printed."""
+    
+    def __repr__(self):
+        """Provide helpful representation when printed."""
 
-		return "< Event event_id={} title={} start_at={} end_at={} >".format(self.event_id, 
-																			 self.title, 
-																			 self.start_at, 
-																			 self.end_at)
+        return "< Event event_id={} title={} start_at={} end_at={} >".format(self.event_id, 
+                                                                             self.title, 
+                                                                             self.start_at, 
+                                                                             self.end_at
+                                                                             )
 
 
 class Invitation(db.Model):
-	"""Invitation."""
+    """Invitation."""
 
 
-	__tablename__ = "invitations"
+    __tablename__ = "invitations"
 
-	invitation_id = db.Column(db.Integer, primary_key=True)
-	invitee_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
-	event_id = db.Column(db.Integer, db.ForeignKey('events.event_id'))
-	attending = db.Column(db.Boolean, nullable=True)
-	notes = db.Column(db.String(128), nullable=True)
+    invitation_id = db.Column(db.Integer, primary_key=True)
+    invitee_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    event_id = db.Column(db.Integer, db.ForeignKey('events.event_id'))
+    attending = db.Column(db.Boolean, nullable=True)
+    notes = db.Column(db.String(128), nullable=True)
 
-	event = db.relationship('Event', backref='invitations')
+    event = db.relationship('Event', backref='invitations')
 
-	invitee = db.relationship('User', backref='invitations')
+    invitee = db.relationship('User', backref='invitations')
 
-
-	# for rating in user.ratings:
-	# 	rating.movie.title
-	# 	rating.score
-
-	# for invitation in event.invitations:
-	# 	invitation.invitee.name
-	# 	invitation.attending
-	# 	invitation.notes
+    # for invitation in event.invitations:
+    #   invitation.invitee.name
+    #   invitation.attending
+    #   invitation.notes
 
 
 ##get back into queue: display everybody invited, and their reply
 
 
-	def __repr__(self):
-		"""Provide helpful representation when printed."""
+    def __repr__(self):
+        """Provide helpful representation when printed."""
 
-		return "< Invitation invitee_id={} creator={} event_id={} attending={} >".format(self.invitee_id, 
-																						 self.creator, 
-																						 self.event_id, 
-																						 self.attending)
+        return "< Invitation invitee_id={} creator={} event_id={} attending={} >".format(self.invitee_id, 
+                                                                                         self.creator, 
+                                                                                         self.event_id, 
+                                                                                         self.attending
+                                                                                         )
 
 
 class Picture(db.Model):
-	"""Posting picture."""
+    """Posting picture."""
 
-	__tablename__ = 'pictures'
+    __tablename__ = 'pictures'
 
-	pic_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-	filename = db.Column(db.String(64), nullable=False)
-	uploader_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
-	event_id = db.Column(db.Integer, db.ForeignKey('events.event_id'))
+    pic_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    filename = db.Column(db.String(64), nullable=False)
+    uploader_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    event_id = db.Column(db.Integer, db.ForeignKey('events.event_id'))
 
-	uploader = db.relationship('User')
-	event = db.relationship("Event", backref="pictures")
+    uploader = db.relationship('User')
+    event = db.relationship("Event", backref="pictures")
 
-	# event = db.relationship('Event')
+    # event = db.relationship('Event')
 
 
-	def __repr__(self):
-		"""Provide helpful representation when printed."""
+    def __repr__(self):
+        """Provide helpful representation when printed."""
 
-		return "< Picture pic_id={} filename={} uploader_id={} event_id={} >".format(self.pic_id, 
-																					 self.filename, 
-																					 self.uploader_id, 
-																					 self.event_id)
+        return "< Picture pic_id={} filename={} uploader_id={} event_id={} >".format(self.pic_id, 
+                                                                                     self.filename, 
+                                                                                     self.uploader_id, 
+                                                                                     self.event_id)
 
 
 class Friendship(db.Model):
-	"""Friending."""
+    """Friending."""
 
-	__tablename__ = 'friendships'
+    __tablename__ = 'friendships'
 
-	friendship_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-	friend_1_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
-	friend_2_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    friendship_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    friend_1_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    friend_2_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
 
-	# user = db.relationship('User')
+    # user = db.relationship('User')
 
 
 
@@ -150,7 +148,7 @@ def connect_to_db(app):
 
 if __name__ == "__main__":
 
-	from server import app
-	connect_to_db(app)
-	db.create_all()
-	print "Connected to DB."
+    from server import app
+    connect_to_db(app)
+    db.create_all()
+    print "Connected to DB."
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 ##### server file ######
-from flask import Flask, render_template, redirect, request, flash, session
+from flask import Flask, render_template, redirect, request, flash, session, g
 from flask_debugtoolbar import DebugToolbarExtension
 
 from model import User, Event, Invitation, Picture, Friendship, connect_to_db, db
@@ -15,127 +15,178 @@ app.secret_key = 'EVENTFULL'
 
 @app.route('/')
 def homepage():
-	"""Show homepage."""
+    """Show homepage."""
 
-	return render_template('homepage.html')
+    return render_template('homepage.html')
+
+# @app.before_request
+# def before_request():
+#   g.session = session
 
 
+# LOGGING IN
+##############################################################
 @app.route('/login')
 def login_form():
-	"""Show login form."""
+    """Show login form."""
 
-	return render_template('login_form.html')
+    return render_template('login_form.html')
 
 
 @app.route('/login', methods=['POST'])
 def login():
-	"""Show login form."""
+    """Show login form."""
 
-	email = request.form.get('user_email')
-	password = request.form.get('user_password')
+    email = request.form.get('user_email')
+    password = request.form.get('user_password')
 
 
-	user = User.query.filter(User.email == email).first()  # returns a user obj
+    user = User.query.filter(User.email == email).first()  # returns a user obj
 
-	if user is not None and user.password == password:
-		session['user_id'] = user.user_id
+    if user is not None and user.password == password:
+        session['user_id'] = user.user_id
 
-		flash('Successfully logged in.')
-		return redirect('/user/{id}'.format(id=user.user_id))
+        flash('Successfully logged in.')
+        return redirect('/user/{id}'.format(id=user.user_id))
 
-	else:
-		flash('Sorry, incorrect login.')
-		return redirect('/login')
+    else:
+        flash('Sorry, incorrect login.')
+        return redirect('/login')
+
+
+# USER PROFILE
+##############################################################
+@app.route('/user')
+def valid_user():
+    """Checks if there's a valid login to redirect"""
+
+    if 'user_id' in session:
+        return redirect('/user/{id}'.format(id=session['user_id']))
+
+    else:
+        flash('Log in first.')
+        return redirect('/login')
 
 
 @app.route('/user/<user_id>')
 def user_profile(user_id):
-	"""User's account page."""
+    """User's account page."""
 
-	user = User.query.filter(User.user_id == user_id).first()
+    user = User.query.filter(User.user_id == user_id).first()
+    invitations = Invitation.query.filter(Invitation.invitee_id == user_id).all()
+    events = Event.query.filter(Event.creator_id == user_id).all()
 
-	invitations = Invitation.query.filter(Invitation.invitee_id == user_id).all()
-
-	events = Event.query.filter(Event.creator_id == user_id).all()
-
-	
-
-	return render_template('user_profile.html', user=user, events=events, invitations=invitations)
+    return render_template('user_profile.html', user=user, events=events, invitations=invitations)
 
 
+# CREATE EVENT
+##############################################################
 @app.route('/create-event')
 def creating_event_form():
-	"""Displays form to create an event."""
+    """Displays form to create an event."""
 
-	return render_template('create_event_form.html')
+    users = User.query.all()
+
+    return render_template('create_event_form.html', users=users)
 
 
 @app.route('/create-event', methods=['POST'])
 def create_event():
-	"""Register new event."""
+    """Register new event."""
 
-	title = request.form.get('title')
-	start_time = request.form.get('start_date')
-	end_time = request.form.get('end_date')
+    title = request.form.get('title')
+    start_time = request.form.get('start_date')
+    end_time = request.form.get('end_date')
 
+    public_str = request.form.get('public')
+    public = public_str == 'public'
 
-	public = request.form.get('public')
-	if public == 'public':
-		public = True
-	else:
-		public = False
-
-
-	new_event = Event(title=title, start_at=start_time, end_at=end_time, creator_id=session['user_id'], public=public)
-	db.session.add(new_event)
-	db.session.commit()
-
-	flash('Event created!')
-
-	return redirect('/event-page/{id}'.format(id=new_event.event_id))
+    new_event = Event(
+        title=title,
+        start_at=start_time, 
+        end_at=end_time,
+        creator_id=session['user_id'], 
+        public=public
+        )
+    db.session.add(new_event)
+    db.session.commit()
 
 
+    friend_invited = request.form.get('friend')
+    invitation = Invitation(
+                            invitee_id=friend_invited,
+                            event_id=new_event.event_id,
+                            )
+
+    db.session.add(invitation)
+    db.session.commit()
+
+    flash('Event created!')
+
+    return redirect('/event-page/{id}'.format(id=new_event.event_id))
+
+
+# EVENT INFORMATION
+##############################################################
 @app.route('/event-page/<event_id>')
 def event_info(event_id):
-	"""Display event info."""
+    """Display event info."""
 
-	event_info = Event.query.filter(Event.event_id == event_id).one()
+    event_info = Event.query.filter(Event.event_id == event_id).one()
 
-	return render_template('event_page.html', event_info=event_info)
+    return render_template('event_page.html', event_info=event_info)
 
 
+# @app.route('/invite')
+# def invitations(event_id):
+#     """Inviting other users."""
+
+#     # for loop to get all the friends checked off...
+
+#     user_id = request.args.get('friend')
+#     invitation = Invitation(invitee_id=user_id, event_id=)
+
+
+
+# NEW USERS
+##############################################################
 @app.route('/registration-form')
 def show_reg_form():
-	"""Displays registration form."""
+    """Displays registration form."""
 
-	return render_template('registration_form.html')
+    return render_template('registration_form.html')
 
 
 @app.route('/new-user', methods=['POST'])
 def validate_user():
-	"""Checks if user is already registered, if not, register new user."""
+    """Checks if user is already registered, if not, register new user."""
 
-	name = request.form.get('new_user_name')
-	email = request.form.get('new_user_email')
-	password = request.form.get('new_user_password')
-	phone = request.form.get('new_user_phone')
+    name = request.form.get('new_user_name')
+    email = request.form.get('new_user_email')
+    password = request.form.get('new_user_password')
+    phone = request.form.get('new_user_phone')
 
-	validation_entry = User.query.filter(User.email == email).first()
+    validation_entry = User.query.filter(User.email == email).first()
 
-	if validation_entry is None:
-		new_user = User(name=name, email=email, password=password, phone=phone)
-		db.session.add(new_user)
-		db.session.commit()
+    if validation_entry is None:
+        new_user = User(
+            name=name,
+            email=email,
+            password=password,
+            phone=phone
+            )
+        db.session.add(new_user)
+        db.session.commit()
 
-		flash('Successfully registered, {name}!'.format(name=name))
-		return redirect('/')
+        flash('Successfully registered, {name}!'.format(name=name))
+        return redirect('/')
 
-	else:
-		flash('Sorry, that email is already in use.')
-		return redirect('/registration-form')
+    else:
+        flash('Sorry, that email is already in use.')
+        return redirect('/registration-form')
 
 
-
+##############################################################
 @app.route('/logout')
 def logout():
     """Logs user out of website."""
